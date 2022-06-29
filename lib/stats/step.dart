9b1bf2b231119b53029537860d8957b08c0cd387
Prateek Gupta <prateek@regami.solutions>
2022-06-29 13:55:29 +0530

    sleep

---
+++ lib/stats/step.dart
@@ -0,0 +1,338 @@
+import 'package:n_tel_care_family_app/landing/landing.dart';
+
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class StatsWidget extends StatefulWidget {
+  const StatsWidget({Key key}) : super(key: key);
+
+  @override
+  _StatsWidgetState createState() => _StatsWidgetState();
+}
+
+class _StatsWidgetState extends State<StatsWidget> {
+  static final List<StepStat> stepStat = [
+    StepStat("10:00", 50, Colors.red),
+    StepStat("11:00", 60, Colors.red),
+    StepStat("12:00", 50, Colors.red),
+    StepStat("13:00", 70, Colors.red),
+    StepStat("14:00", 80, Colors.red),
+    StepStat("15:00", 40, Colors.red),
+    StepStat("16:00", 50, Colors.red),
+    StepStat("17:00", 80, Colors.red),
+    StepStat("18:00", 70, Colors.red),
+  ];
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  var color1 = Color(0xFF00B89F);
+  var color2 = Color(0xFF1A1A1A);
+  var color3 = Color(0xFF1A1A1A);
+  DateTime dateTime;
+  @override
+  Widget build(BuildContext context) {
+    List<charts.Series<StepStat, String>> step = [
+      charts.Series(
+          data: stepStat,
+          id: "Heart Rate",
+          domainFn: (StepStat pops, _) => pops.Time,
+          measureFn: (StepStat pops, _) => pops.Stat,
+          colorFn: (StepStat pops, _) =>
+              charts.ColorUtil.fromDartColor(pops.line))
+    ];
+
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Color(0xFF1F252B),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Stack(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset(
+                      'assets/images/8679613_footprint_line_icon_.png',
+                      width: 220,
+                      height: 220,
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(10, 63, 0, 0),
+                      child: InkWell(
+                        onTap: () async {
+                          await Navigator.pop(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    ModifiedLandingPageWidget(),
+                              ));
+                        },
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Icon(
+                              Icons.chevron_left_sharp,
+                              color: Color(0xFFAFAFAF),
+                              size: 45,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                          child: Text(
+                            'Steps',
+                            style:
+                                FlutterFlowTheme.of(context).bodyText1.override(
+                                      fontFamily: 'Montserrat',
+                                      color: Color(0xFFAFAFAF),
+                                      fontSize: 35,
+                                      fontWeight: FontWeight.w200,
+                                    ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(50, 25, 50, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: InkWell(
+                                onTap: () async {
+                                  setState(() {
+                                    color1 = Color(0xFF00B89F);
+                                    color2 = Color(0xFF1A1A1A);
+                                    color3 = Color(0xFF1A1A1A);
+                                  });
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: color1,
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(10),
+                                      bottomRight: Radius.circular(0),
+                                      topLeft: Radius.circular(10),
+                                      topRight: Radius.circular(0),
+                                    ),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsetsDirectional.fromSTEB(
+                                        0, 5, 0, 5),
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          'Daily',
+                                          style: FlutterFlowTheme.of(context)
+                                              .bodyText1
+                                              .override(
+                                                fontFamily: 'Montserrat',
+                                                color: Color(0xFF6D6767),
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w200,
+                                              ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                )),
+                          ),
+                          Expanded(
+                              child: InkWell(
+                            onTap: () async {
+                              setState(() {
+                                color1 = Color(0xFF1A1A1A);
+                                color2 = Color(0xFF00B89F);
+                                color3 = Color(0xFF1A1A1A);
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: color2,
+                                borderRadius: BorderRadius.circular(0),
+                              ),
+                              child: Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'Weekly',
+                                      style: FlutterFlowTheme.of(context)
+                                          .bodyText1
+                                          .override(
+                                            fontFamily: 'Montserrat',
+                                            color: Color(0xFF6D6767),
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w200,
+                                          ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )),
+                          Expanded(
+                            child: InkWell(
+                              onTap: () async {
+                                setState(() {
+                                  color1 = Color(0xFF1A1A1A);
+                                  color2 = Color(0xFF1A1A1A);
+                                  color3 = Color(0xFF00B89F);
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: color3,
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(0),
+                                    bottomRight: Radius.circular(10),
+                                    topLeft: Radius.circular(0),
+                                    topRight: Radius.circular(10),
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 5, 0, 5),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'Monthly',
+                                        style: FlutterFlowTheme.of(context)
+                                            .bodyText1
+                                            .override(
+                                              fontFamily: 'Montserrat',
+                                              color: Color(0xFF6D6767),
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w200,
+                                            ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(5, 10, 5, 10),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              showDatePicker(
+                                      context: context,
+                                      initialDate: dateTime == null
+                                          ? DateTime.now()
+                                          : dateTime,
+                                      firstDate: DateTime(2001),
+                                      lastDate: DateTime.now())
+                                  .then((date) {
+                                setState(() {
+                                  dateTime = date;
+                                });
+                              });
+                            },
+                            icon: Icon(
+                              Icons.chevron_left,
+                              color: Color(0xFFAFAFAF),
+                              size: 32,
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              dateTime == null
+                                  ? DateTime.now().toString()
+                                  : dateTime.toString(),
+                              textAlign: TextAlign.center,
+                              style: FlutterFlowTheme.of(context)
+                                  .bodyText1
+                                  .override(
+                                    fontFamily: 'Poppins',
+                                    color: Color(0xFFAFAFAF),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              showDatePicker(
+                                      context: context,
+                                      initialDate: dateTime == null
+                                          ? DateTime.now()
+                                          : dateTime,
+                                      firstDate: DateTime(2001),
+                                      lastDate: DateTime.now())
+                                  .then((date) {
+                                setState(() {
+                                  dateTime = date;
+                                });
+                              });
+                            },
+                            icon: Icon(
+                              Icons.chevron_right,
+                              color: Color(0xFFAFAFAF),
+                              size: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 0),
+                        child: Container(
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height / 3,
+                          decoration: BoxDecoration(
+                              color: Color(0xFF272E36),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: charts.BarChart(step),
+                        )),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StepStat {
+  final String Time;
+  final int Stat;
+  final Color line;
+
+  StepStat(this.Time, this.Stat, this.line);
+}
