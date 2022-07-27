0bf481072a46c0e777d2861bd5f2687842c2467d
DESKTOP-GMIKO3J\priya <Priya@regami.solutions>
2022-07-27 10:31:00 +0530

    weekly graph integrated

--- lib/stats/heart.dart
+++ lib/stats/heart.dart
@@ -4,12 +4,12 @@ import 'dart:ui';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:n_tel_care_family_app/backend/api_requests/api_calls.dart';
 import 'package:n_tel_care_family_app/landing/landing.dart';
-
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:month_year_picker/month_year_picker.dart';
 
@@ -22,25 +22,84 @@ class StatsWidget extends StatefulWidget {
 }
 
 DateTime dateTime;
+DateTime dateTimeWeek = DateTime.now();
 dynamic Hrate;
 bool daily = true;
 bool weekly = false;
 bool monthly = false;
+DateTime _startDate = DateTime.now().subtract(Duration(days: 7));
+DateTime _endDate = DateTime.now();
+DateTimeRange dateRange = DateTimeRange(start: _startDate, end: _endDate);
 
 class _StatsWidgetState extends State<StatsWidget> {
+  bool checkLimit(DateTime limitDay) {
+    if (limitDay.isBefore(_startDate)) {
+      return false;
+    } else if (limitDay.isAfter(_endDate)) {
+      return false;
+    }
+    return true;
+  }
+
+  Future displayDateRangePicker(context) async {
+    DateTimeRange newRange = await showDateRangePicker(
+      context: context,
+      initialDateRange: dateRange,
+      firstDate: DateTime(dateTimeWeek.year - 7),
+      lastDate: dateTimeWeek,
+    );
+    if (newRange == null) {
+      return;
+    }
+    setState(() {
+      dateRange = newRange;
+    });
+  }
+
   Future<dynamic> HeartStatus;
+  Future<List<dynamic>> SList;
+
   List<HeartStat> hRate = [];
+  List<HeartStatWeekMax> hRateMax = [];
+  List<HeartStatWeekMax> hRateMin = [];
   GetHrate getHrate = GetHrate();
   String id;
+  String wDate;
+
   _StatsWidgetState(this.id);
+
+  List<charts.Series<HeartStatWeekMax, String>> _createSampleData() {
+    return [
+      new charts.Series<HeartStatWeekMax, String>(
+          id: 'max',
+          domainFn: (HeartStatWeekMax sales, _) => sales.day,
+          measureFn: (HeartStatWeekMax sales, _) => sales.Stat,
+          data: hRateMax,
+          colorFn: (_, __) => charts.Color.fromHex(code: "#00B89F"),
+          labelAccessorFn: (HeartStatWeekMax sales, _) =>
+              '\$${sales.Stat.toString()}'),
+      new charts.Series<HeartStatWeekMax, String>(
+          id: 'min',
+          domainFn: (HeartStatWeekMax sales, _) => sales.day,
+          measureFn: (HeartStatWeekMax sales, _) => sales.Stat,
+          data: hRateMin,
+          colorFn: (_, __) => charts.Color.fromHex(code: "#272E36"),
+          labelAccessorFn: (HeartStatWeekMax sales, _) =>
+              '\$${sales.Stat.toString()}'),
+    ];
+  }
+
   @override
   void initState() {
     super.initState();
     getHeartRate();
+    getHeartWeekRate();
+    // SList = fetchSList();
     // int versionCode = BuildConfig.VERSION_CODE;
     // HeartStatus = fetchStat();
   }
 
+//final List<charts.Series<HeartStatWeekMax, String>> seriesList = _createSampleData();
   void getHeartRate() async {
     var response = await getHrate
         .get('http://18.208.148.208:4000/graph/health_status/?senior_id=${id}');
@@ -57,6 +116,35 @@ class _StatsWidgetState extends State<StatsWidget> {
     });
     print(hRate);
   }
+
+  void getHeartWeekRate() async {
+    var response = await getHrate.get(
+        'http://18.208.148.208:4000/graph/health_status/heart_rate/weekly?date=${wDate}&senior_id=${id}');
+    print(response.statusCode);
+    print(response.body.runtimeType);
+    print(response.body);
+    final rate = jsonDecode((response.body));
+    print(rate["heart_rate"]);
+    List<HeartStatWeekMax> temp = heartStatWeekMaxFromJson(rate["heart_rate"]);
+    setState(() {
+      hRateMax = temp;
+    });
+    List<HeartStatWeekMax> temp1 = heartStatWeekMinFromJson(rate["heart_rate"]);
+    setState(() {
+      hRateMin = temp1;
+    });
+
+    print(hRate);
+  }
+
+/*  Future<List<dynamic>> fetchSList() async {
+    String wDate = DateFormat('yyyy-MM-dd').format(dateTimeWeek);
+    print(wDate);
+    final ApiCallResponse SList = await GetHeartWeek.call(id: id, date: wDate);
+    print(SList.statusCode);
+    print(SList.jsonBody);
+    return SList.jsonBody;
+  }*/
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
   var color1 = Color(0xFF00B89F);
@@ -161,6 +249,9 @@ class _StatsWidgetState extends State<StatsWidget> {
                                       color1 = Color(0xFF00B89F);
                                       color2 = Color(0xFF1A1A1A);
                                       color3 = Color(0xFF1A1A1A);
+                                      daily = true;
+                                      weekly = false;
+                                      monthly = false;
                                     });
                                   },
                                   child: Container(
@@ -200,14 +291,13 @@ class _StatsWidgetState extends State<StatsWidget> {
                             Expanded(
                                 child: InkWell(
                               onTap: () async {
-                                if (weekly == true) {
-                                  weekly = false;
-                                } else
-                                  weekly = false;
                                 setState(() {
                                   color1 = Color(0xFF1A1A1A);
                                   color2 = Color(0xFF00B89F);
                                   color3 = Color(0xFF1A1A1A);
+                                  weekly = true;
+                                  daily = false;
+                                  monthly = false;
                                 });
                               },
                               child: Container(
@@ -246,6 +336,9 @@ class _StatsWidgetState extends State<StatsWidget> {
                                     color1 = Color(0xFF1A1A1A);
                                     color2 = Color(0xFF1A1A1A);
                                     color3 = Color(0xFF00B89F);
+                                    monthly = true;
+                                    daily = false;
+                                    weekly = false;
                                   });
                                 },
                                 child: Container(
@@ -293,6 +386,7 @@ class _StatsWidgetState extends State<StatsWidget> {
                         padding: EdgeInsetsDirectional.fromSTEB(5, 10, 5, 10),
                         child: Row(
                           mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             IconButton(
                               onPressed: () {
@@ -316,23 +410,47 @@ class _StatsWidgetState extends State<StatsWidget> {
                                 size: 32,
                               ),
                             ),
-                            Expanded(
-                              child: Text(
-                                dateTime == null
-                                    ? DateFormat('dd-MM-yyyy')
-                                        .format(DateTime.now())
-                                    : DateFormat('dd-MM-yyyy').format(dateTime),
-                                textAlign: TextAlign.center,
-                                style: FlutterFlowTheme.of(context)
-                                    .bodyText1
-                                    .override(
-                                      fontFamily: 'Poppins',
-                                      color: Color(0xFFAFAFAF),
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w300,
-                                    ),
+                            if (daily ?? true)
+                              Expanded(
+                                child: Text(
+                                  dateTime == null
+                                      ? DateFormat('dd-MM-yyyy')
+                                          .format(DateTime.now())
+                                      : DateFormat('dd-MM-yyyy')
+                                          .format(dateTime),
+                                  textAlign: TextAlign.center,
+                                  style: FlutterFlowTheme.of(context)
+                                      .bodyText1
+                                      .override(
+                                        fontFamily: 'Poppins',
+                                        color: Color(0xFFAFAFAF),
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                ),
                               ),
-                            ),
+                            if (weekly == true)
+                              Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      5, 10, 5, 10),
+                                  child: InkWell(
+                                    onTap: () async {
+                                      await displayDateRangePicker(context);
+                                    },
+                                    child: Text(
+                                      '${DateFormat('d / MM / y').format(_startDate)} - ${DateFormat('d / MM / y').format(_endDate)}',
+                                      style: FlutterFlowTheme.of(context)
+                                          .bodyText1
+                                          .override(
+                                            fontFamily: 'Poppins',
+                                            color: Color(0xFFAFAFAF),
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w300,
+                                          ),
+
+                                      //style: kWhite20RegularTextStyle,
+                                    ),
+                                  )),
                             IconButton(
                               onPressed: () {
                                 showDatePicker(
@@ -357,46 +475,105 @@ class _StatsWidgetState extends State<StatsWidget> {
                           ],
                         ),
                       ),
-                      Padding(
-                          padding:
-                              EdgeInsetsDirectional.fromSTEB(10, 10, 10, 0),
-                          child: Container(
-                            width: double.infinity,
-                            height: MediaQuery.of(context).size.height / 3,
-                            decoration: BoxDecoration(
-                                color: Color(0xFF272E36),
-                                borderRadius: BorderRadius.circular(10)),
-                            child: charts.LineChart(
-                              heart,
-                              domainAxis: new charts.NumericAxisSpec(
-                                  tickProviderSpec:
-                                      charts.BasicNumericTickProviderSpec(
-                                          zeroBound: false),
-                                  renderSpec: new charts.SmallTickRendererSpec(
+                      if (daily == true)
+                        Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(10, 10, 10, 0),
+                            child: Container(
+                              width: double.infinity,
+                              height: MediaQuery.of(context).size.height / 3,
+                              decoration: BoxDecoration(
+                                  color: Color(0xFF272E36),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: charts.LineChart(
+                                heart,
+                                domainAxis: new charts.NumericAxisSpec(
+                                    tickProviderSpec:
+                                        charts.BasicNumericTickProviderSpec(
+                                            zeroBound: false),
+                                    renderSpec:
+                                        new charts.SmallTickRendererSpec(
+                                      labelStyle: new charts.TextStyleSpec(
+                                          fontSize: 8, // size in Pts.
+                                          color: charts.MaterialPalette.white),
+                                    )),
+                                primaryMeasureAxis: new charts.NumericAxisSpec(
+                                    renderSpec: new charts.GridlineRendererSpec(
+                                  labelStyle: new charts.TextStyleSpec(
+                                      fontSize: 8, // size in Pts.
+                                      color: charts.MaterialPalette.white),
+                                )),
+
+                                // domainAxis: charts.OrdinalAxisSpec(
+                                //     renderSpec: charts.SmallTickRendererSpec(
+                                //         labelStyle: new charts.TextStyleSpec(
+                                //             color: charts.MaterialPalette.white))),
+                              ),
+                            )
+                            // Image.asset(
+                            //   'assets/images/heartRate.png',
+                            //   width: 400,
+                            //   height: 400,
+                            //   fit: BoxFit.fill,
+                            // ),
+                            ),
+                      if (weekly == true && daily == false)
+                        Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(10, 10, 10, 0),
+                            child: Container(
+                                width: double.infinity,
+                                height: MediaQuery.of(context).size.height / 3,
+                                decoration: BoxDecoration(
+                                    color: Color(0xFF272E36),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: charts.BarChart(
+                                  _createSampleData(),
+                                  domainAxis: new charts.OrdinalAxisSpec(
+                                      renderSpec:
+                                          new charts.SmallTickRendererSpec(
+                                    labelStyle: new charts.TextStyleSpec(
+                                        fontSize: 6, // size in Pts.
+                                        color: charts.MaterialPalette.white),
+                                  )),
+                                  /*  primaryMeasureAxis:
+                                      new charts.NumericAxisSpec(
+                                          renderSpec:
+                                              new charts.GridlineRendererSpec(
                                     labelStyle: new charts.TextStyleSpec(
                                         fontSize: 8, // size in Pts.
                                         color: charts.MaterialPalette.white),
-                                  )),
-                              primaryMeasureAxis: new charts.NumericAxisSpec(
-                                  renderSpec: new charts.GridlineRendererSpec(
-                                labelStyle: new charts.TextStyleSpec(
-                                    fontSize: 8, // size in Pts.
-                                    color: charts.MaterialPalette.white),
-                              )),
-
-                              // domainAxis: charts.OrdinalAxisSpec(
-                              //     renderSpec: charts.SmallTickRendererSpec(
-                              //         labelStyle: new charts.TextStyleSpec(
-                              //             color: charts.MaterialPalette.white))),
-                            ),
-                          )
-                          // Image.asset(
-                          //   'assets/images/heartRate.png',
-                          //   width: 400,
-                          //   height: 400,
-                          //   fit: BoxFit.fill,
-                          // ),
-                          ),
+                                  )),*/
+                                  animate: true,
+                                  defaultRenderer: new charts.BarRendererConfig(
+                                      groupingType:
+                                          charts.BarGroupingType.stacked,
+                                      strokeWidthPx: 0.5,
+                                      maxBarWidthPx: 10),
+                                )
+                                //barGroupingType:  charts.BarGroupingType.stacked, strokeWidthPx: 2.0)),
+                                )),
+                      if (weekly == false && daily == false && monthly == true)
+                        Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(10, 10, 10, 0),
+                            child: Container(
+                                width: double.infinity,
+                                height: MediaQuery.of(context).size.height / 3,
+                                decoration: BoxDecoration(
+                                    color: Color(0xFF272E36),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: charts.BarChart(
+                                  _createSampleData(),
+                                  animate: true,
+                                  defaultRenderer: new charts.BarRendererConfig(
+                                      groupingType:
+                                          charts.BarGroupingType.stacked,
+                                      strokeWidthPx: 0.5,
+                                      maxBarWidthPx: 10),
+                                )
+                                //barGroupingType:  charts.BarGroupingType.stacked, strokeWidthPx: 2.0)),
+                                )),
                       Padding(
                         padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
                         child: Row(
@@ -609,10 +786,36 @@ List<HeartStat> heartStatFromJson(List<dynamic> rate) {
   });
   return heart;
 }
+
+List<HeartStatWeekMax> heartStatWeekMaxFromJson(List<dynamic> rate) {
+  List<HeartStatWeekMax> heart = [];
+
+  rate.forEach((element) {
+    heart.add(HeartStatWeekMax(element["date"], element["max_min"]));
+  });
+  return heart;
+}
+
+List<HeartStatWeekMax> heartStatWeekMinFromJson(List<dynamic> rate) {
+  List<HeartStatWeekMax> heart = [];
+
+  rate.forEach((element) {
+    heart.add(HeartStatWeekMax(element["date"], element["min"]));
+  });
+  return heart;
+}
+
 // List<HeartStat>.from(json.decode(str).map((x) => HeartStat.fromJson(x)));
 
 String heartStatToJson(List<HeartStat> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class HeartStatWeekMax {
+  final String day;
+  final int Stat;
+
+  HeartStatWeekMax(this.day, this.Stat);
+}
 
 class HeartStat {
   HeartStat({
