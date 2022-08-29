dfd546fa1792e1a21046493ac15a843df9efb677
AniketNandi <aniket.regami@solutions>
2022-08-30 01:01:04 +0530

    dashboard and edit prfile bug fixes

--- lib/landing/landing.dart
+++ lib/landing/landing.dart
@@ -111,7 +111,7 @@ class _ModifiedLandingPageWidgetState extends State<ModifiedLandingPageWidget> {
     print('dashBoardLoading : $_isDashboardDataLoading');
     return RefreshIndicator(
       onRefresh: () async {
-        await fetchDashData(selectedId);
+        await _fetchDashBoardData(_seniorsList[isSelected]["id"]);
       },
       child: Scaffold(
         key: scaffoldKey,
