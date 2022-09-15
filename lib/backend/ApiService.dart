572131c2748751b883d4df190396f1a02ae5ada7
AniketNandi <aniket.regami@solutions>
2022-09-16 00:15:17 +0530

    minor update

--- lib/backend/ApiService.dart
+++ lib/backend/ApiService.dart
@@ -1,9 +1,6 @@
 class ApiService {
   static final bool _debug = false;
 
-  // static final String _productionUrl =
-  //     'https://netlcare-admin-api.regamicompass.com';
-
   static final String _productionUrl = 'https://api.ntelcare.com';
   static final String _testingUrl = 'http://18.208.148.208:4000';
 
