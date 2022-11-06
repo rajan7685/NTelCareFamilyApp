a6208a53daad96ef9c476f850e6a2db38440911e
DESKTOP-UO66FST\K G T <aniket@regami.sultions>
2022-11-06 16:13:32 +0530

    build issue fix

--- lib/new_user/add.dart
+++ lib/new_user/add.dart
@@ -705,30 +705,7 @@ class _AddWidgetState extends State<Add> {
                                       ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(4, 10, 4, 0),
-                          child: Container(
-                            width: double.maxFinite,
-                            decoration: BoxDecoration(
-                              color: Color(0xFFEEEEEE),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: Padding(
-                              padding:
-                                  EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
-                              child: TextFormField(
-                                controller: textController7,
-                                obscureText: false,
-                                onChanged: (_) => _loadAddress(),
-                                // onSaved: (newValue) => print(newValue),
-                                decoration: InputDecoration(
-                                  labelText: 'Zip Code',
-                                  labelStyle: FlutterFlowTheme.of(context)
+                                  style: FlutterFlowTheme.of(context)
                                       .bodyText1
                                       .override(
                                         fontFamily: 'Poppins',
